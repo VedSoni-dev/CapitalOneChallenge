d197527c67d216cb11f974cd2807cb9d2e528b86
vedantn06soni@gmail.com <your.email@example.com>
2024-11-10 02:16:25 -0600

    New UI and FrontPage for Info

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,6 @@
+
+
+
 class FinancialGame {
     constructor() {
         this.balance = 0;
--- static/js/utils.js
+++ static/js/utils.js
@@ -117,6 +117,7 @@ class GameUtils {
                 return false;
             }
         }
+        
     };
 }
 
